5ecb0c9fb7fb0046d4acbb961a6847234d25f5a9
Attila_Tapai <attila.tapai01@gmail.com>
2023-10-24 10:09:01 +0200

    updated "about me" page

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -1,28 +1,76 @@
 "Use client";
 // Import required components and libraries
 import { User, Code, Briefcase, GraduationCap } from "lucide-react";
-import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
-// Sample data
 const sections = [
   {
     icon: <User size={20} />,
     label: "About Me",
-    content:
-      "I am Attila, a passionate full-stack developer with experience in building dynamic and responsive web applications.",
+    content: (
+      <>
+        At 39, I embarked on a transformative journey, steering away from the logistics and immediacies of supply chain management to immerse myself into the captivating realm of
+        coding.
+        <br />
+        This shift wasn't just a professional transition; it was a personal evolution.
+        <br />
+        <br />
+        While coding presents its own set of challenges, for me, it evokes the same thrill I find in gaming, a space where every problem is a level waiting to be conquered.
+        <br />
+        The decision wasn't easy, especially considering my extensive experience in supply chain, where I:
+        <br />
+        <br />
+        - Seamlessly managed multiple system interfaces.
+        <br />
+        - Nurtured and evolved supplier relationships over years.
+        <br />
+        - Tackled countless line stop scenarios, always ensuring continuity.
+        <br />
+        <br />
+        Outside the professional sphere, I am a voracious reader—sci-fi and fantasy being my genres of choice. They whisk me away to different worlds, much like coding does in its
+        own unique way.
+        <br />
+        Socializing, gaming, and long, reflective walks are my go-to activities for rejuvenation.
+        <br />
+        <br />
+        One of the pivotal challenges I've faced recently has been balancing work, rigorous learning at Greenfox Academy, and precious moments with family and friends. But the
+        crowning achievement of this period? Definitely 'nexTicket', our group project. It stands as a testament to our collective passion, determination, and camaraderie.
+      </>
+    ),
   },
   {
     icon: <Code size={20} />,
     label: "Skills",
-    content: "JavaScript, Typescript, Next.js, Node.js, Express.js, Puppeteer, mySQL, Tailwind, Styled Components, CSS, HTML.",
+    content: <>JavaScript, Typescript, Next.js, Node.js, Express.js, Puppeteer, mySQL, Tailwind, Styled Components, CSS, HTML.</>,
   },
   {
     icon: <Briefcase size={20} />,
     label: "Experiences",
-    content:
-      "Currently, I am actively seeking my inaugural role in software development. Prior to embarking on this journey, I held pivotal roles in esteemed corporations such as BP Inc., KUKA AG, and Harman International. Within the realm of Supply Chain management, my positions as Material Group Manager, Sourcing Lead, and Critical Parts Manager allowed me to drive strategic procurement initiatives, oversee demand forecasting, optimize inventory levels, implement Just-In-Time (JIT) strategies, enhance vendor relationship management, and employ Total Cost of Ownership (TCO) analysis. My responsibilities also encompassed improving end-to-end supply chain visibility, working in cross-functional teams to streamline sourcing processes, and deploying risk mitigation strategies to ensure supply continuity in volatile markets.",
+    content: (
+      <>
+        Currently, I am actively seeking my inaugural role in software development. Prior to embarking on this journey, I held pivotal roles in esteemed corporations such as BP
+        Inc., KUKA AG, and Harman International.
+        <br />
+        Within the realm of Supply Chain management, my positions as Material Group Manager, Sourcing Lead, and Critical Parts Manager allowed me to drive strategic procurement
+        initiatives:
+        <br />
+        <br />
+        - Oversee demand forecasting
+        <br />
+        - Optimize inventory levels
+        <br />
+        - Implement Just-In-Time (JIT) strategies
+        <br />
+        - Enhance vendor relationship management
+        <br />
+        - Employ Total Cost of Ownership (TCO) analysis.
+        <br />
+        <br />
+        My responsibilities also encompassed improving end-to-end supply chain visibility, working in cross-functional teams to streamline sourcing processes, and deploying risk
+        mitigation strategies to ensure supply continuity in volatile markets.
+      </>
+    ),
   },
   {
     icon: <GraduationCap size={20} />,
@@ -33,9 +81,9 @@ const sections = [
         <br />
         <br />
         Kecskemét University of Mechanical Engineering - Technical Manager
-		<br />
         <br />
-		Csonka János Technical High School - Logistics
+        <br />
+        Csonka János Technical High School - Logistics
       </>
     ),
   },
@@ -50,9 +98,9 @@ export default function BioPage() {
           {sections.map((s) => (
             <div className='mb-8'>
               {" "}
-              {/* This div will give the desired margin between cards */}
+              {/* This div gives margin between cards */}
               <Card>
-                <div className='p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16'>
+                <div className='p-4 relative flex flex-col items-center md:text-justify gap-4 duration-700 group md:gap-8 md:py-12 lg:pb-24 md:p-8'>
                   <span
                     className='absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent'
                     aria-hidden='true'
@@ -60,9 +108,9 @@ export default function BioPage() {
                   <span className='relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange'>
                     {s.icon}
                   </span>{" "}
-                  <div className='z-10 flex flex-col items-center'>
+                  <div className='z-10 flex flex-col items-center md:text-center lg:text-center'>
                     <span className='lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display'>{s.label}</span>
-                    <span className='mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200'>{s.content}</span>
+                    <span className='mt-4 text-sm sm:text-justify md:text-center duration-1000 text-zinc-400 group-hover:text-zinc-200'>{s.content}</span>
                   </div>
                 </div>
               </Card>
--- app/page.tsx
+++ app/page.tsx
@@ -3,49 +3,51 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "Projects", href: "/projects" },
-	{ name: "Contact", href: "/contact" },
-	{ name: "About me", href: "/aboutMe" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+  { name: "About me", href: "/aboutMe" },
 ];
 
 export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<nav className="my-16 animate-fade-in">
-				<ul className="flex flex-wrap items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
-				</ul>
-			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<Particles
-				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
-			/>
-			<h1 className="z-10 text-3xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-7xl whitespace-nowrap bg-clip-text ">
-				Hi! My name is Attila Tápai
-			</h1>
+  return (
+    <div className='flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black'>
+      <nav className='my-16 animate-fade-in'>
+        <ul className='flex flex-wrap items-center justify-center gap-4'>
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className='text-sm duration-500 text-zinc-500 hover:text-zinc-300'
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
+      <Particles
+        className='absolute inset-0 -z-10 animate-fade-in'
+        quantity={100}
+      />
+      <h1 className='z-10 text-3xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-7xl whitespace-nowrap bg-clip-text '>
+        Hi! My name is Attila Tápai.
+      </h1>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 mx-6">
-				...and this is a showcase of my work. I'm actively broadening my expertise in full stack technologies like {" "}
-					<Link
-						target="_blank"
-						href="https://https://nextjs.org/"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						Next.js
-					</Link>
-				</h2>
-			</div>
-		</div>
-	);
+      <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
+      <div className='my-16 text-center animate-fade-in'>
+        <h2 className='text-sm text-zinc-500 mx-6'>
+          Dive into a curated showcase of my projects, reflecting my growing passion for full-stack development. Currently, I'm delving deep into technologies like{" "}
+          <Link
+            target='_blank'
+            href='https://https://nextjs.org/'
+            className='underline duration-500 hover:text-zinc-300'
+          >
+            Next.js
+          </Link>
+          <br />
+          As a recent graduate eager to learn and adapt, I'm on the lookout for opportunities to harness my skills and contribute to innovative teams.
+        </h2>
+      </div>
+    </div>
+  );
 }
